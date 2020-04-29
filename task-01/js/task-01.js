4c0b3fa9be1471b859c1855473be47564f65dd1e
Andriy Kyrylenko <z9877969@ukr.net>
2020-04-29 21:09:23 +0300

    redefines toString for alert, but not for console.log

--- task-01/js/task-01.js
+++ task-01/js/task-01.js
@@ -1,14 +1,10 @@
-// "use strict";
+"use strict";
 
 const StringBuilder = class StringBuilder {
   constructor(baseString = "") {
     this.value = baseString;
-    // this.toString.call(this);
-    // return this.value.toString();
   }
 };
-
-// StringBuilder.prototype.advValue = "";
 
 StringBuilder.prototype.append = function (str) {
   this.value = this.value + str;
@@ -25,44 +21,12 @@ StringBuilder.prototype.pad = function (str) {
   return this;
 };
 
-// StringBuilder.prototype.toString = function toStringValue() {
-//   console.log("toString");
-//   return this.value;
-// };
-// StringBuilder.prototype.valueOf = function toValueOf() {
-//   console.log("valueOf");
-//   return this.value;
-// };
-
-// console.log = function (s) {
-//   console.log(s + "");
-// };
-
-const ToPrimitive = {
-  [Symbol.toPrimitive](hint) {
-    switch (hint) {
-      case "number":
-        return this.value.valueOf();
-      case "string":
-      default:
-        return this.value.toString();
-    }
-  },
-  valueOf() {
-    return this.num;
-  },
-  toString() {
-    return this.value;
-  },
+StringBuilder.prototype.toString = function toStringValue() {
+  return this.value;
 };
-
-StringBuilder.prototype = Object.create(ToPrimitive);
-
-// console.log = (a) => {};
 
 const builder = new StringBuilder(".");
 builder.append("^").prepend("^").pad("=");
 
-alert(builder);
 console.log(builder);
-console.log([Symbol.toPrimitive]);
+alert(builder);
